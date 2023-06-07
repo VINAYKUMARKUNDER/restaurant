f4199744a68e62a693a3b3d1aa30a069dccc3c03
Vinay Kumar <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-06-07 17:29:43 +0530

    ok

--- server/routers/appConfig.js
+++ server/routers/appConfig.js
@@ -9,7 +9,15 @@ const expenseCategoriesRoutes = require('../modules/expenseCategories/expenseCat
 const expensesRoutes = require('../modules/expenses/expenses.controller');
 const foodCategoriesRoutes = require('../modules/foodCategories/foodCategories.controller');
 const messagesRoutes = require('../modules/messages/messages.controller');
-const menusRoutes = require('../modules/menus/menus.controller')
+const menusRoutes = require('../modules/menus/menus.controller');
+const migrationsRoutes = require('../modules/migrations/migrations.controller');
+const newAdminsRoutes = require('../modules/newAdmins/newAdmins.controller');
+const ordersRoutes = require('../modules/orders/orders.controller');
+const password_resetsRoutes = require('../modules/password_resets/password_resets.controller');
+const reservAtionsRoutes = require('../modules/reservAtions/reservAtions.controller');
+const staffsRoutes = require('../modules/staffs/staffs.controller');
+const usersRoutes = require('../modules/users/users.controller');
+
 
 
 
@@ -21,6 +29,13 @@ app.use('/api/v1/expenses', expensesRoutes);
 app.use('/api/v1/foodcategories', foodCategoriesRoutes);
 app.use('/api/v1/message', messagesRoutes);
 app.use('/api/v1/menus', menusRoutes);
+app.use('/api/v1/migrations', migrationsRoutes);
+app.use('/api/v1/newadmin', newAdminsRoutes);
+app.use('/api/v1/orders', ordersRoutes);
+app.use('/api/v1/passwordr', password_resetsRoutes);
+app.use('/api/v1/reservactions', reservAtionsRoutes);
+app.use('/api/v1/staffs', staffsRoutes);
+app.use('/api/v1/users', usersRoutes);
 
 
 
