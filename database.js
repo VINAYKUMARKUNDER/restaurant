b52ea8a800220b6b254cfb27122624d57b5b4960
Vinay Kumar <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-06-14 11:05:01 +0530

    agen push

--- database.js
+++ database.js
@@ -2,13 +2,17 @@
 const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
 
+const DB_NAME = process.env.DB_NAME || 'restaurant';
+const DB_PASSWORD=  process.env.DB_PASSWORD || 'Vinay@1313';
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_USERNAME =     process.env.DB_USERNAME || 'root';
 
 const conn = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
+  DB_NAME,
+  DB_USERNAME,
+   DB_PASSWORD,
     {
-      host: process.env.DB_HOST,
+      host: DB_HOST ,
       dialect: "mysql",
     }
   );
